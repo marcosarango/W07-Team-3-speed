870fa212de0ad71043bb882b8b3dd3a35e0a7d57
marcosarango <sar21005@byui.edu>
2021-10-28 21:41:55 -0400

    added score file

--- speed/game/actor.py
+++ speed/game/actor.py
@@ -1,9 +1,20 @@
 
 
-class actor:
+class Actor:
 
     def __init__(self):
 
         self._text = ""
         self._position = point(0,0)
         self._velocity = Point(0,0)
+    
+    def get_position(self):
+        return self._position
+
+    def get_text(self):
+        return self._text
+
+    def get_velocity(self):
+        return self._velocity
+    
+    
